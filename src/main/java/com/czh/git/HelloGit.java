b7cee664a68e589da4ac02276f2043397ccc8c14
Kurumi <czh2298580658@gmail.com>
2023-01-19 15:36:52 +0800

    第二次更新

--- src/main/java/com/czh/git/HelloGit.java
+++ src/main/java/com/czh/git/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("CNM");
         System.out.println("第一次修改");
+        System.out.println("第二次修改");
     }
 }
